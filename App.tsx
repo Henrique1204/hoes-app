c223fa56da7b93a61ef5a0baee309105636408ec
Paulo Souza <paulosouza@HL-SP1016.local>
2024-01-04 22:20:57 -0300

    instalar oneSignal no projeto e configuracao inicial

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
 
-import { Routes } from './src/routes';
+import OneSignal from "react-native-onesignal";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { NativeBaseProvider } from "native-base";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { Routes } from "./src/routes";
+
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
+
+import { CartContextProvider } from "./src/contexts/CartContext";
+
+OneSignal.setAppId(process.env.EXPO_PUBLIC_API_URL!);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
